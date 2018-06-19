91559b5eb3a9434026cb3d92666377f7d4765e09
allan hung <hung.allan@gmail.com>
2018-06-19 06:34:38 -0400

    test

--- main.go
+++ main.go
@@ -1,67 +1,52 @@
-// Copyright 2016 LINE Corporation
-//
-// LINE Corporation licenses this file to you under the Apache License,
-// version 2.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
 package main
 
 import (
-  "html"
-  "fmt"
-  "log"
-  "net/http"
-  "os"
-  "github.com/line/line-bot-sdk-go/linebot"
+    "log"
+    "net/http"
+    "os"
+
+    "github.com/gin-gonic/gin"
+    "github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-  bot, err := linebot.New(
-    os.Getenv("CHANNEL_SECRET"),
-    os.Getenv("CHANNEL_TOKEN"),
-  )
-  if err != nil {
-    log.Fatal(err)
-  }
+    port := os.Getenv("PORT")
 
-  http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-  // Setup HTTP Server for receiving requests from LINE platform
-  http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-    events, err := bot.ParseRequest(req)
+    bot, err := linebot.New(
+        os.Getenv("CHANNEL_SECRET"),
+        os.Getenv("CHANNEL_TOKEN"),
+    )
     if err != nil {
-      if err == linebot.ErrInvalidSignature {
-        w.WriteHeader(400)
-      } else {
-        w.WriteHeader(500)
-      }
-      return
+        log.Fatal(err)
     }
-    for _, event := range events {
-      if event.Type == linebot.EventTypeMessage {
-        switch message := event.Message.(type) {
-        case *linebot.TextMessage:
-          reply_message := fmt.Sprintf("%s[%s]", "test", message.Text)
-          if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply_message)).Do(); err != nil {
-            log.Print(err)
-          }
+
+    router := gin.New()
+    router.Use(gin.Logger())
+
+    router.POST("/callback", func(c *gin.Context) {
+        events, err := bot.ParseRequest(c.Request)
+        if err != nil {
+            if err == linebot.ErrInvalidSignature {
+                log.Print(err)
+            }
+            return
         }
-      }
-    }
-  })
-  // This is just a sample code.
-  // For actually use, you must support HTTPS by using `ListenAndServeTLS`, reverse proxy or etc.
-  if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-    log.Fatal(err)
-  }
+        for _, event := range events {
+            if event.Type == linebot.EventTypeMessage {
+                switch message := event.Message.(type) {
+                case *linebot.TextMessage:
+                    reply_msg := fmt.Sprintf("You Type:  %s", message.Text)
+                    if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+                        log.Print(err)
+                    }
+                }
+            }
+        }
+    })
+
+    router.Run(":" + port)
 }
